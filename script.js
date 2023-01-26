2ea035aa3c648455fa19cf7fec6a518a06614095
Vinicius Bacellar <dev_bacellar@outlook.com>
2023-01-26 00:11:35 -0300

    Atualização no script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // FOOTER
 const footer = document.getElementById('footer')
 const actualYear = new Date().getFullYear().toString()
-footer.innerText = `\u00a9 2022-${actualYear} Vinícius Bacellar. Todos os direitos reservados.`
+footer.innerHTML = `\u00a9 2022-${actualYear} Vinícius Bacellar. Todos os direitos reservados.`
