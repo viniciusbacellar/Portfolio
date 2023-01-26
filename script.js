6b988f7c708f57ada3b0dcc73a494602eefc1beb
Vinicius Bacellar <dev_bacellar@outlook.com>
2023-01-26 00:02:45 -0300

    Automatizacao da data do copyright

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// FOOTER
+const footer = document.getElementById('footer')
+const actualYear = new Date().getFullYear().toString()
+footer.innerText = `\u00a9 2022-${actualYear} Vinícius Bacellar. Todos os direitos reservados.`
